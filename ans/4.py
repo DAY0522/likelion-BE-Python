34479034c54f31856e694f4e7aa29c04c5d7f46d
Jinuk Hong <jinuk0613@gmail.com>
2023-04-05 20:24:10 +0900

    4번

--- ans/4.py
+++ ans/4.py
@@ -1,7 +1,26 @@
 # 4. 
 
+
+def count(List):
+    #print(List)
+    a=[]
+    for i in List:
+        if 'e' in i:
+            #print(i)
+            continue
+        else:
+            a.append(i)
+    tupa=tuple(a)
+    print(tupa)
+
+
+
+
+  
 animals = ('lion', 'tiger', 'penguin', 'dog', 'cat')
 
-
-
+list1=list(animals)
+#print(list1)
+count(list1)
+#print(list1)
 
