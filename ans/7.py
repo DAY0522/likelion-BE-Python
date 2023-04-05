73a3aa7eef7aae5cbab8beac137c5b44efc220ee
Jinuk Hong <jinuk0613@gmail.com>
2023-04-05 20:56:37 +0900

    7번

--- ans/7.py
+++ ans/7.py
@@ -1,9 +1,17 @@
 # 7.
 
+from movies import*
+import random
 
 
-# class RandomCinema():
-    
+class RandomCinema(Movie,Cinema):
+    na=input('영화 입력:' )
+    Mov=Movie(na,'몰라','홍진욱')
+    ci=Cinema()
+    #print(f"{self.cinema_list}")
+    r=random.randint(0,2)
+    print(f"{ci.cinema_list[r]}") 
+
 
 
 
