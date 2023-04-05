b488603c5bb1bdbd206358257cf6e4026277f2a0
Boo <boojh811@gmail.com>
2023-04-05 19:59:56 +0900

    commit3

--- ans/2.py
+++ ans/2.py
@@ -1,6 +1,11 @@
 # 2. 
+i = 0
 
-
+for i in range(1,4):
+    name = input("이름을 입력해주세요: ")
+    dep = input("학과를 입력해주세요: ")
+    num = input("학번을 입력해주세요: ")
+    print(f"{name}({num})님의 학과는 {dep}입니다.")
     
 
 
